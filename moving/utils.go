ba6668444994a2944b541153f1a045932d153ed3
qfittere <quentin.fitte-rey@etu.utc.fr>
2023-11-09 19:03:19 +0100

    add of priorityqueue and psuedo code of astar

---
+++ moving/utils.go
@@ -0,0 +1,31 @@
+package moving
+
+type Item struct { // Une fois Agent crée stocker des agents avec priorité
+	value    interface{}
+	priority int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
