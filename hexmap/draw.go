753452080eba0abc91596f5186969df5b791765b
Adrien Simon <adrisim2001@gmail.com>
2023-11-10 11:37:28 +0100

    fixes

--- hexmap/draw.go
+++ hexmap/draw.go
@@ -1,7 +1,6 @@
 package hexmap
 
 import (
-	"fmt"
 	"golang.org/x/image/colornames"
 	_ "image/png"
 	"log"
@@ -55,7 +54,6 @@ func (g *GameMap) DrawHex(background *ebiten.Image, xCenter float32, yCenter flo
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale((3/2.0)*float64(hexSize)/float64(hexImage.Bounds().Dx()), (4.0/3.0)*float64(hexSize)/float64(hexImage.Bounds().Dy()))
 	op.GeoM.Translate(float64(xCenter-(3.0/4.0)*hexSize), float64(yCenter-(2.0/3.0)*hexSize))
-	fmt.Println(biome, hexImage.Bounds().Dx(), hexImage.Bounds().Dy())
 	background.DrawImage(hexImage, op)
 
 	x0 := xCenter
@@ -65,10 +63,10 @@ func (g *GameMap) DrawHex(background *ebiten.Image, xCenter float32, yCenter flo
 	y2 := yCenter + hexSize/2
 	y3 := yCenter - hexSize/4
 	y4 := yCenter + hexSize/4
-	vector.StrokeLine(background, x1, y3, x1, y4, 1, colornames.Black, false)
-	vector.StrokeLine(background, x1, y4, x0, y2, 1, colornames.Black, false)
-	vector.StrokeLine(background, x0, y2, x2, y4, 1, colornames.Black, false)
-	vector.StrokeLine(background, x2, y4, x2, y3, 1, colornames.Black, false)
-	vector.StrokeLine(background, x2, y3, x0, y1, 1, colornames.Black, false)
-	vector.StrokeLine(background, x0, y1, x1, y3, 1, colornames.Black, false)
+	vector.StrokeLine(background, x1, y3, x1, y4, 0, colornames.Black, false)
+	vector.StrokeLine(background, x1, y4, x0, y2, 0, colornames.Black, false)
+	vector.StrokeLine(background, x0, y2, x2, y4, 0, colornames.Black, false)
+	vector.StrokeLine(background, x2, y4, x2, y3, 0, colornames.Black, false)
+	vector.StrokeLine(background, x2, y3, x0, y1, 0, colornames.Black, false)
+	vector.StrokeLine(background, x0, y1, x1, y3, 0, colornames.Black, false)
 }
