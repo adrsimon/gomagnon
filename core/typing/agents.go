b68d1d436a81572f0c9603367d898581d7b35854
jlescieu <jean.lescieux@etu.utc.fr>
2023-11-13 17:43:25 +0100

    first commit, waiting for A* algo

--- core/typing/agents.go
+++ core/typing/agents.go
@@ -1,17 +1,24 @@
 package typing
 
-type Human struct {
-	id          int
-	Position    string
-	Type        rune // can be cromagnon or neandertal
-	Hungriness  int  // 0 to 100
-	Thirstiness int  // 0 to 100
-	Age         int
-	Gender      rune
-	Strength    int // 0 to 100
-	Sociability int // 0 to 100
-}
+import (
+	"fmt"
+	"math/rand"
+)
 
+type Human struct {
+	id             int
+	Position       string
+	Type           rune // can be cromagnon or neandertal
+	Hungriness     int  // 0 to 100
+	Thirstiness    int  // 0 to 100
+	Age            int
+	Gender         rune
+	Strength       int // 0 to 100
+	Sociability    int // 0 to 100
+	CurrentPath    []*Hexagone
+	MovingToTarget bool
+	Target         *Hexagone
+}
 
 const (
 	AnimalFoodValueMultiplier = 3.0
@@ -34,4 +41,44 @@ func (h *Human) EvaluateSurroundings(hex *Hexagone) float64 {
 	}
 
 	return score
+}
+
+// Adapt to quentin A* + add concurency handling
+func (h *Human) UpdateAgent() {
+	if !h.MovingToTarget {
+		surroundingHexagons := h.GetSurroundingHexagons()
+		targetHexagon := h.ChooseTargetHexagon(surroundingHexagons)
+
+		if targetHexagon != nil {
+			h.CurrentPath = AStarPathfinding(h.CurrentHexagon(), targetHexagon)
+			h.Target = targetHexagon
+			h.MovingToTarget = true
+		}
+	}
+
+	if h.MovingToTarget && len(h.CurrentPath) > 0 {
+		nextHexagon := h.CurrentPath[0]
+		h.MoveToHexagon(nextHexagon)
+		h.CurrentPath = h.CurrentPath[1:]
+		h.UpdateStateBasedOnResource(nextHexagon)
+		if nextHexagon == h.Target {
+			h.MovingToTarget = false
+		}
+	}
+}
+
+func (h *Human) UpdateStateBasedOnResource(hex *Hexagone) {
+	if hex.Resource == ANIMAL {
+		h.Hungriness = max(0, h.Hungriness-rand.Intn(20))
+	}
+	if hex.Resource == FRUIT {
+		h.Hungriness = max(0, h.Hungriness-rand.Intn(10))
+	}
+	if hex.Biome.BiomeType == WATER {
+		h.Thirstiness = max(0, h.Thirstiness-rand.Intn(30))
+	}
+}
+
+func (h *Human) MoveToHexagon(hex *Hexagone) {
+	h.Position = fmt.Sprintf("%d:%d", hex.Position.X, hex.Position.Y)
 }
--- simulation/update.go
+++ simulation/update.go
@@ -1,5 +1,21 @@
 package simulation
 
+import (
+	"sync"
+
+	"github.com/adrsimon/gomagnon/core/typing"
+)
+
 func (s *Simulation) Update() error {
+	var wg sync.WaitGroup
+	for _, agent := range s.GameMap.Board.Agents {
+		wg.Add(1)
+		go func(a *typing.Human) {
+			defer wg.Done()
+			a.UpdateAgent()
+			// handle errors
+		}(agent)
+	}
+	wg.Wait()
 	return nil
 }
