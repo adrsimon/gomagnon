ffe005df6e7f1e5c3dd2cf7ece91f1ac37ac7899
Adrien Simon <adrisim2001@gmail.com>
2023-11-20 14:28:36 +0100

    panning and zooming

--- simulation/draw.go
+++ simulation/draw.go
@@ -6,7 +6,33 @@ import (
 )
 
 func (s *Simulation) Draw(screen *ebiten.Image) {
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		s.cameraX += 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		s.cameraX -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		s.cameraY += 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		s.cameraY -= 2
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyComma) {
+		s.zoomFactor += 0.005
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyPeriod) {
+		s.zoomFactor -= 0.005
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyR) {
+		s.cameraX = 0
+		s.cameraY = 0
+		s.zoomFactor = 1
+	}
+
 	screen.Fill(s.GameMap.BackgroundColor)
-	drawing.DrawBoard(screen, s.GameMap.Board)
-	drawing.DrawAgents(screen, s.GameMap.Board)
+	drawing.DrawBoard(screen, s.GameMap.Board, s.cameraX, s.cameraY, s.zoomFactor)
+	drawing.DrawAgents(screen, s.GameMap.Board, s.cameraX, s.cameraY, s.zoomFactor)
 }
