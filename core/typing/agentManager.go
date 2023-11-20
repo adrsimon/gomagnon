e5351f13f2dd82577b00ad9e4a3f53cbbb3dc8b8
Adrien Simon <adrisim2001@gmail.com>
2023-11-20 10:13:26 +0100

    using agent manager to take resources

--- core/typing/agentManager.go
+++ core/typing/agentManager.go
@@ -1,5 +1,7 @@
 package typing
 
+import "fmt"
+
 type agentToManager struct {
 	AgentID string
 	Action  string
@@ -14,13 +16,13 @@ type managerToAgent struct {
 }
 
 type AgentManager struct {
-	Map          map[string]*Hexagone
+	Map          *map[string]*Hexagone
 	messIn       chan agentToManager
 	stackRequest []agentToManager
 	Agents       map[string]*Human
 }
 
-func NewAgentManager(Map map[string]*Hexagone, messIn chan agentToManager, stackRequest []agentToManager, agents map[string]*Human) *AgentManager {
+func NewAgentManager(Map *map[string]*Hexagone, messIn chan agentToManager, stackRequest []agentToManager, agents map[string]*Human) *AgentManager {
 	return &AgentManager{Map: Map, messIn: messIn, stackRequest: stackRequest, Agents: agents}
 }
 
@@ -40,37 +42,23 @@ func (agMan *AgentManager) startAnswering() {
 		}
 	}
 }
-func (agMan *AgentManager) removeAgent(hexagone string, a *Human) {
-	ind := -1
-	for i, v := range agMan.Map[hexagone].Agents {
-		if v == a {
-			ind = i
-		}
-	}
-	if ind != -1 {
-		agMan.Map[hexagone].Agents = append(agMan.Map[hexagone].Agents[:ind], agMan.Map[hexagone].Agents[ind+1:]...)
-	}
-}
 
 func (agMan *AgentManager) execute(request agentToManager) {
 	switch request.Action {
-	case "walk":
-		agMan.Map[request.Pos].Agents = append(agMan.Map[request.Pos].Agents, agMan.Agents[request.AgentID])
-		agMan.removeAgent(request.Pos, agMan.Agents[request.AgentID])
-		request.commOut <- managerToAgent{Valid: true, Map: agMan.Map, Resource: NONE}
 	case "get":
-		switch agMan.Map[request.Pos].Resource {
-		case 1:
-			request.commOut <- managerToAgent{Valid: false, Map: agMan.Map, Resource: NONE}
+		switch (*agMan.Map)[request.Pos].Resource {
+		case NONE:
+			request.commOut <- managerToAgent{Valid: false, Map: *agMan.Map, Resource: NONE}
 		default:
-			res := agMan.Map[request.Pos].Resource
-			agMan.Map[request.Pos].Resource = NONE
-			request.commOut <- managerToAgent{Valid: true, Map: agMan.Map, Resource: res}
+			res := (*agMan.Map)[request.Pos].Resource
+			(*agMan.Map)[request.Pos].Resource = NONE
+			request.commOut <- managerToAgent{Valid: true, Map: *agMan.Map, Resource: res}
 		}
 	}
 }
 
 func (agMan *AgentManager) Start() {
+	fmt.Println("Starting agent manager")
 	go agMan.startListening()
 	go agMan.startAnswering()
 }
