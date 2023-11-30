bd13aff590024fa82a90abac79664f70e1acb0c5
adrien <adrisim2001@gmail.com>
2023-11-30 19:25:27 +0100

    delete gamemap structure (#33)

---
+++ core/typing/hexagones.go
@@ -0,0 +1,28 @@
+package typing
+
+import (
+	"fmt"
+)
+
+type Hexagone struct {
+	Position *Point2D
+	Resource ResourceType
+	Biome    BiomeType
+	Agents   []*Human
+	Hut      *Hut
+}
+
+func (h *Hexagone) EvenRToAxial() (int, int) {
+	q := h.Position.X - ((h.Position.Y + (h.Position.Y & 1)) / 2)
+	r := h.Position.Y
+	return q, r
+}
+
+func (h *Hexagone) ToString() string {
+	return fmt.Sprintf("%d:%d", h.Position.X, h.Position.Y)
+}
+
+type Point2D struct {
+	X int
+	Y int
+}
